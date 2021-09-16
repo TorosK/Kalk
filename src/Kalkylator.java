6c308dc5e0713aaaaa87b82504ae4779d7875976
Toros Kutlu <toros_kutlu@hotmail.com>
2021-09-16 19:56:04 +0200

    3rd commit (branch)

--- src/Kalkylator.java
+++ src/Kalkylator.java
@@ -1,13 +1,15 @@
 // Grupparbeiten
+// Grupp Matrix
 // Toros
+//
 import java.util.Scanner;
 public class Kalkylator {
 
     //Creates global constant variables that cannot be changed as minimum and maximum numbers for input purposes
-    final static int MINIMUM = 0;
-    final static int MAXIMUM = 1000;
+    final static double MINIMUM = 0;
+    final static double MAXIMUM = 1000;
 
-    //Creates global variables for percentMethod, so they can be displayed to user
+    //Creates global variables that can be used to verify numbers
     static double num = 0;
     static double percentage = 0;
 
@@ -19,69 +21,73 @@ public class Kalkylator {
         boolean menuLoop = true;
         while (menuLoop == true) {
 
+            try {
 
-            System.out.println("Please choose: ");
-            System.out.println("\n1) Square Root ");
-            System.out.println("2) Percent Calculation ");
-            System.out.println("3) Pythagorean Theorem ");
-            System.out.println("4) Area of a circle ");
-            System.out.println("5) Exit ");
-            System.out.println("\n***************************");
-            int menuChoice = in.nextInt();
+                System.out.println("Please choose: ");
+                System.out.println("\n1) Square Root ");
+                System.out.println("2) Percent Calculation ");
+                System.out.println("3) Pythagorean Theorem ");
+                System.out.println("4) Area of a circle ");
+                System.out.println("5) Exit ");
+                System.out.println("\n***************************");
 
-            if (menuChoice == 1) {
-                System.out.println("***Square root menu***\n\nPlease input a whole number: ");
-                int validNum = getVal(MINIMUM, MAXIMUM);
-                sqrtOf(validNum);
-                System.out.println("The square root of " + validNum + " is " + sqrtOf(validNum));
-                System.out.println("\nMake input to return to main menu...");
-                in.next();
+                int menuChoice = in.nextInt();
 
-            } else if (menuChoice == 2) {
-                // int validNum = getVal(MINIMUM, MAXIMUM);
-                double result = percent();
-                System.out.println(percentage + " percent of " + num + " is " + result);
-                System.out.println("\nMake input to return to main menu...");
-                in.next();
+                if (menuChoice == 1) {
+                    System.out.println("***Square root menu***\n\nPlease input a number: ");
+                    double validNum = getVal(MINIMUM, MAXIMUM);
+                    sqrtOf(validNum);
+                    if (validNum != ) {
 
-            } else if (menuChoice == 3) {
-                System.out.println("Please write the length of one of the square sides of the triangle: ");
-                double validNum = getVal(MINIMUM, MAXIMUM);
-                System.out.println("Please write the length of the other square side of the triangle: ");
-                double validNum2 = getVal(MINIMUM, MAXIMUM);
-                double result = pyth(validNum, validNum2);
-                if (result != 0) {
-                    System.out.println("The result is: " + result);
+                    }
+                    System.out.println("The square root of " + validNum + " is " + sqrtOf(validNum));
+                    System.out.println("\nMake input to return to main menu...");
+                    in.next();
+
+                } else if (menuChoice == 2) {
+                    // int validNum = getVal(MINIMUM, MAXIMUM);
+                    double result = percent();
+                    System.out.println(percentage + " percent of " + num + " is " + result);
+                    System.out.println("\nMake input to return to main menu...");
+                    in.next();
+
+                } else if (menuChoice == 3) {
+                    System.out.println("Please write the length of one of the square sides of the triangle: ");
+                    double validNum = getVal(MINIMUM, MAXIMUM);
+                    System.out.println("Please write the length of the other square side of the triangle: ");
+                    double validNum2 = getVal(MINIMUM, MAXIMUM);
+                    double result = pyth(validNum, validNum2);
+                    if (result != 0) {
+                        System.out.println("The result is: " + result);
+                    } else if (result == 0) {
+
+                    }
+                    System.out.println("\nMake input to return to main menu...");
+                    in.next();
+
+                } else if (menuChoice == 4) {
+                    System.out.println("Please enter the diameter of the circle in numbers only: ");
+                    double validNum = getVal(MINIMUM, MAXIMUM);
+                    double result = circleA(validNum);
+                    if (result != 0) {
+                        System.out.println("The full circle area is: " + result);
+                    } else if (result == 0) {
+
+                    }
+                    System.out.println("\nMake input to return to main menu...");
+                    in.next();
+                } else if (menuChoice == 5) {
+                    System.out.println("Shutting down...");
+                    menuLoop = false;
+                } else {
+                    System.out.println("ERROR wrongful input!");
                 }
-                else if (result == 0 ){
-
-                }
-                System.out.println("\nMake input to return to main menu...");
-                in.next();
-
-            } else if (menuChoice == 4) {
-                System.out.println("Please enter the diameter of the circle in numbers only: ");
-                double validNum = getVal(MINIMUM, MAXIMUM);
-                double result = circleA(validNum);
-                if (result != 0) {
-                    System.out.println("The full circle area is: " + result);
-                }
-                else if (result == 0 ){
-
-                }
-                System.out.println("\nMake input to return to main menu...");
+            }
+            catch (Exception e) {
+                System.out.println("Wrongful input! Try again...");
                 in.next();
             }
-            else if (menuChoice == 5) {
-                System.out.println("Shutting down...");
-                menuLoop = false;
-            }
-            else {
-                System.out.println("ERROR wrongful input!");
-            }
-
         }
-
     }
 
     public static double circleA (double x){
@@ -126,7 +132,6 @@ public class Kalkylator {
             }
         }
         catch (Exception e){
-
             System.out.println("Error, please enter a number!");
         }
         return x;
@@ -135,30 +140,31 @@ public class Kalkylator {
     public static double getVal ( double min, double max){
         double x = 0;
         try {
+
             x = in.nextDouble();
 
             if (x < min) {
-                //System.out.println("Please input a positive number");
+                System.out.println("Please input a positive number");
             } else if (x > max) {
-                //System.out.println("Please input a smaller number than 1000");
+                System.out.println("Please input a smaller number than 1000");
             }
         }
         catch (Exception e){
-
-            System.out.println("Error, please enter a number!");
+            System.out.println("Error, please enter a number!\n\n(Please enter comma for decimal number)\nMake any input to return to main menu");
+            in.next();
         }
         return x;
     }
 
-    public static double checkInputError(double x) {
+    /*public static double checkInputError(double x) {
         if (x == 0) {
             System.out.println("Error!");
         }
         else if (x != 0) {
-           // return x;
+            // return x;
         }
         return x;
-    }
+    } */
 
 
 
---
+++ src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        double x = 14.5;
+        System.out.println(x);
+        String comma = Double.toString(x); // "14.5"
+        System.out.println(comma);
+        comma.replace(".", ","); //string_name.replace(old_string, new_string)
+        String s = "Toros";
+        s.replace("T", "S");
+        System.out.println(s);
+        // 14.5 = 14,5
+        System.out.println(comma);
+        double y = Double.parseDouble(comma); //double str1 = Double.parseDouble(str);
+        System.out.println(y);
+    }
+}
